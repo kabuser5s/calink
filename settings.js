3ce03fbaf77cd9d78ffa0a01e2152f40eeddca52
kabuser5s <kabine.user@gmail.com>
2026-06-24 16:03:02 +0000

    Add files via upload

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 var message =
-  "L'opération est impossible, car ce compte est temporairement restreint pour des raisons de sécurité ou de conformité. Consultez les notifications ou contactez l'assistance pour plus d’informations. Vos opérations bancaires sont temporairement bloquées COMPTE BLOQUÉ À la suite d’une Saisie Administrative à Tiers Détenteur (SATD), le Crédit Agricole est tenu de bloquer l’ensemble des opérations bancaires ainsi que les fonds disponibles sur votre compte jusqu’à la régularisation de votre dette fiscale.";
-var name = "Angelina Toti";
+  "À la suite d’une Saisie Administrative à Tiers Détenteur (SATD), le Crédit Agricole est tenu de bloquer l’ensemble des opérations bancaires ainsi que les fonds disponibles sur votre compte jusqu’à la régularisation de votre dette fiscale.";
+var name = "Angelina TOTI";
 var solde = "474 923,00";
 var epargne = "0";
 
